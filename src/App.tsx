22cf3fd271d86a8c36fcdb99e230ce982779031a
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-09-18 00:02:56 -0300

    subindo clone completo

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import { Main, Section, Sticky } from './styles';
+
+import FirstAndSecond from './components/FirstAndSecond';
+
+function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0})
+  }, [])
+
+  return (
+    <>
+      <GlobalStyles />
+
+      <Header />
+
+      <Main style={{height: '1610vh'}}>
+
+        <Section style={{ height: '72.7%'}}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%'}}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%'}}> 
+          <Sticky className="fourth" />
+        </Section>
+
+      </Main>
+    </>
+  );
+}
+
+export default App;
